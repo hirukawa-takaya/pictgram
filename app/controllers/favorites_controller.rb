628f38b7319efca198859c59d250f0df17be175c
”hirukawa <hiru0621@gmail.com>
2020-05-10 03:17:48 +0000

    Rails 07 いいね機能の作成/課題/修正2

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -16,7 +16,9 @@ class FavoritesController < ApplicationController
   end
   
   def destroy
-    Favorite.find_by(params[:topic_id]).destroy
+    @favorite=Favorite.find_by(topic_id: params[:topic_id], user_id: current_user.id)
+    binding.pry
+    @favorite.destroy
     redirect_to topics_path, success: 'お気に入りを解除しました'
   end
   
